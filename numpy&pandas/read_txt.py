5fef528171976d7295a6a4b7b067d6252f60429c
xiangkejun <kjunxiang@126.com>
2019-03-01 12:18:03 +0800

    a

---
+++ numpy&pandas/read_txt.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+my_font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc",size=16)
+pose = pd.read_table("F:/AI/python_xx/xy.txt",sep=',')
+pose.plot.scatter(x='x',y='y')
+#print(changjing_3['field.pose.pose.position.x'])
+
+plt.show()
