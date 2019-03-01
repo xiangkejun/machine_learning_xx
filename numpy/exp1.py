b326a2c67a569bf57e98451f9db829193c773787
xiangkejun <kjunxiang@126.com>
2019-03-01 13:29:36 +0800

    Update exp1.py

--- numpy/exp1.py
+++ numpy/exp1.py
@@ -1,3 +1,38 @@
 #xx
 
 import numpy as np
+# a = np.arange(15).reshape(3,5)
+# print(a)
+# print(a.shape)
+# print(a.ndim)
+# print(a.size)
+# print(a.dtype.name)
+# print(type(a))
+
+# b = np.array([2,3,4])
+# b = np.array([(1.3,2,3),(4,5,6)])
+# print(b)
+# print(b.dtype)
+
+# c = np.array([[1,2],[3,4]],dtype = complex)
+# print(c)
+
+# d = np.zeros((3,4))
+# print(d)
+
+# e = np.ones((2,3,4), dtype= np.int16)
+# print(e)
+
+# d = np.empty((2,3))
+# print(d)
+
+# f = np.arange(10,30,5)
+# f = np.arange(0,2,0.3)
+# print(f)
+
+from numpy import pi
+g = np.linspace(0,2,9)
+print(g)
+h = np.linspace(0,2*pi,100)
+j = np.sin(h)
+print(j)
