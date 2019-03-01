4cf42c9bc70891108890386e757bdb268ea6e62a
xiangkejun <kjunxiang@126.com>
2019-03-01 19:47:08 +0800

    Update exp6.py

--- numpy/exp6.py
+++ numpy/exp6.py
@@ -8,3 +8,8 @@ print(np.linalg.inv(a)) # 求逆
 
 u = np.eye(2) # 单位阵
 print(u)
+print(np.trace(u)) #对角线的和 2.0
+j = np.array([[0.0,-1.0],[1.0,0.0]])
+y = np.array([[5.],[7.]])
+print(np.linalg.solve(a,y)) # 求ax=y的解
+print(np.linalg.eig(j)) # 求特征值和特征向量
