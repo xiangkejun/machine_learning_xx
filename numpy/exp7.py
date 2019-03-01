e1023ae62a0471ea0b9e95af57d1fe89d7737523
xiangkejun <kjunxiang@126.com>
2019-03-01 19:59:11 +0800

    Update exp7.py

--- numpy/exp7.py
+++ numpy/exp7.py
@@ -1,6 +1,13 @@
 # xx
 #  技巧
 import numpy as np
+
 a = np.arange(30)
 a.shape = 2,-1,3  # -1自动规划形状
 print(a.shape)
+
+# 矢量堆叠
+x = np.arange(0,10,2)
+y = np.arange(5)
+print(np.vstack([x,y]))
+print(np.hstack([x,y]))
