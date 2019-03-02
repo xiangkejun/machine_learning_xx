04fa7d2d8a6583633c3289631406c123956aba44
xiangkejun <kjunxiang@126.com>
2019-03-02 14:39:49 +0800

    Update cnn_torch.py

--- neural_net/cnn_torch.py
+++ neural_net/cnn_torch.py
@@ -3,7 +3,7 @@ View more, visit my tutorial page: https://morvanzhou.github.io/tutorials/
 My Youtube Channel: https://www.youtube.com/user/MorvanZhou
 
 Dependencies:
-torch: 0.4
+torch: 1.0
 torchvision
 matplotlib
 """
