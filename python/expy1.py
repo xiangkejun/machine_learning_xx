d783e5a88ac9e5a1d4e7ba0ba5f73064e092484c
xiangkejun <kjunxiang@126.com>
2019-03-04 13:41:18 +0800

    Update expy1.py

--- python/expy1.py
+++ python/expy1.py
@@ -3,6 +3,6 @@
 # for a in range(1,5):
 #     print(a)  # 1,2,3,4
 
-for a,b in enumerate(['a','b']):
-    print(a)
-    print(b)
+for a,b in enumerate(['a','b']): 
+    print(a)  # 0 1
+    print(b)  # a b
