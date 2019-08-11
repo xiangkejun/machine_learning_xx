c77d56b3ca2e0545a6d77a22898498a25b73d4d0
xiangkejun <kjunxiang@126.com>
2019-08-11 10:35:18 +0800

    N

--- numpy&pandas/read_txt.py
+++ numpy&pandas/read_txt.py
@@ -5,8 +5,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 
+
 my_font = FontProperties(fname=r"c:\windows\fonts\simsun.ttc",size=16)
-pose = pd.read_table("F:/AI/python_xx/xy.txt",sep=',')
+pose = pd.read_table("F:/AI/python_xx/xy.csv",sep=',')
+
 pose.plot.scatter(x='x',y='y')
 
 plt.show()
