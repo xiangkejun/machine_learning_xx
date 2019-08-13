1dddaa5616cf02b63d295cc899a7ce7d035cafe2
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-08-13 14:42:39 +0800

    Update and rename minist_tf.py to fashion_minist_tf.py

--- tf2_xx/fashion_minist_tf.py
+++ tf2_xx/fashion_minist_tf.py
@@ -9,7 +9,11 @@ import tensorflow as tf
 
 (train_image,train_label),(test_image,test_label) = tf.keras.datasets.fashion_mnist.load_data()
 
-plt.imshow(train_image[0])
+plt.imshow(train_image[0]) # 显示第一张图片
+plt.show()
+
+print(train_label[:10]) # [9 0 0 3 0 2 7 2 5 5]
+
 
 train_image = train_image / 255 
 test_image = test_image /255 
@@ -29,3 +33,6 @@ model.compile(optimizer='adam',
 model.fit(train_image,train_label,epochs=5)
 
 print(model.evaluate(test_image,test_label))  #
+
+print(model.predict(train_image[:1])) # 对第一张图片进行测试
+# [[0.00200258 0.00096263 0.00356959 0.01052908 0.0028083  0.1917256 0.01329381 0.39358088 0.05934311 0.32218438]]
