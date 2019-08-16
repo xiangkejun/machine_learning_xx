79820406666729b5935bd6447d201ac9e9a9a455
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-08-16 10:14:32 +0800

    Add files via upload

---
+++ numpy&pandas/plt_test1.py
@@ -0,0 +1,16 @@
+#encoding=utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__' :
+    t1 = np.arange(0, 30, 0.1)
+    plt.figure()
+    plt.ion()
+    for i in range(100):
+        plt.ylim(-10, 10) #此处限制了一下y轴坐标最大最小值，防止刻度变化，不利于观察。
+        plt.plot(t1, 0.1*i*np.sin(t1 + 0.1 * i))
+        plt.plot(t1, 0.1 * (i -100) * np.cos(t1 + 0.2 * i))
+        plt.pause(0.01)
+        plt.clf()
+    plt.ioff()
