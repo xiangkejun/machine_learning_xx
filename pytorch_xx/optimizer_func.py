4ede4ec60285b207b7070271dc8741692261ed93
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-08-16 17:58:45 +0800

    Add files via upload

---
+++ pytorch_xx/optimizer_func.py
@@ -0,0 +1,23 @@
+#encoding=utf-8
+# 求函数取的最小值 的 x
+import numpy as np 
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import pyplot as plt 
+import torch
+
+# (x-5)**2
+def loss_f(x):
+    return x**2 - 10*x + 25
+
+x = torch.tensor([9.],requires_grad=True)
+optimizer = torch.optim.Adam([x],lr=1e-3)
+for step in range(20000):
+    pred = loss_f(x)
+
+    optimizer.zero_grad()
+    pred.backward()
+    optimizer.step()
+
+    if step % 200 == 0:
+        print('step {}: x = {},f(x) = {}'
+            .format(step,x.tolist(), pred.item()))
