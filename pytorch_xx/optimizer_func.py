e3393862b95c29f5230a7c43a2e612336c5fb82b
xiangkejun <42697766+xiangkejun@users.noreply.github.com>
2019-08-16 18:30:17 +0800

    Update optimizer_func.py

--- pytorch_xx/optimizer_func.py
+++ pytorch_xx/optimizer_func.py
@@ -6,10 +6,16 @@ from matplotlib import pyplot as plt
 import torch
 
 # (x-5)**2
-def loss_f(x):
-    return x**2 - 10*x + 25
+# def loss_f(x):
+#     return x**2 - 10*x + 25
 
-x = torch.tensor([9.],requires_grad=True) # 随机初始值
+# 多元函数
+# (x-y)**2 = 25
+# x**2 +y**2-2*x*y -25 
+def loss_f(x):
+    return x[0]**2 + x[1]**2 -2*x[0]*x[1] - 25
+
+x = torch.tensor([2.,1.],requires_grad=True) # 随机初始值
 optimizer = torch.optim.Adam([x],lr=1e-3)
 # optimizer = torch.optim.SGD([x],lr=0.01)
 # optimizer = torch.optim.RMSprop([x],lr=1e-3)
